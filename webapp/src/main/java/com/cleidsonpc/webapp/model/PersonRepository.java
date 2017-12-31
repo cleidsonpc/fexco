57aba02b1724d9bfcdc1b4bc40d929f3ffd1ee61
Cleidson <Cleidson@192.168.15.5>
2017-12-31 13:03:03 -0200

    Changing project location

--- webapp/src/main/java/com/cleidsonpc/webapp/model/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cleidsonpc.webapp.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, Long> {
-
-	public Person findById(Long id);
-	
-}
